3d68f984e5d8cc19b71b7b0e212ec2e2b9e5ad59
lubensheng <60734715+lubensheng@users.noreply.github.com>
2022-01-05 18:07:31 +0800

    修改reduc

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './reducers/userReducer';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {userReducer},
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -3,29 +3,32 @@ import {
   login
 } from '../../service/index'
 import {
-  loginType
+  loginType,
 } from '../../types'
 
-const fetchUserById = createAsyncThunk(
-  'users/fetchByIdStatus',
+
+
+const getUserInfo = createAsyncThunk(
+  'getUserInfo/getUserInfoStatus',
   async (userInfo: loginType, thunkAPI) => {
     const response = await login(userInfo);
-    return response.data
+    return response.data;
   }
 )
 
-// Then, handle actions in your reducers:
-// const usersSlice = createSlice({
-//   name: 'users',
-//   initialState: { entities: [], loading: 'idle' },
-//   reducers: {
-//     // standard reducer logic, with auto-generated action types per reducer
-//   },
-//   extraReducers: (builder) => {
-//     // Add reducers for additional action types here, and handle loading state as needed
-//     builder.addCase(fetchUserById.fulfilled, (state, action) => {
-//       // Add user to the state array
-//       state.entities.push(action.payload)
-//     })
-//   },
-// })
+let initialState = {
+  data: {}
+}
+
+const usersSlice = createSlice({
+  name: 'getUserInfo',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserInfo.fulfilled, (state, action) => {
+        state.data = action.payload;
+    });
+  },
+})
+
+export default usersSlice.reducer
